6fbf6b33dc794d5678882b335a188d917ccdf1f0
Lokno Decker <lokno1@gmail.com>
2019-12-21 21:54:05 -0500

    Initial Commit

---
+++ idle.py
@@ -0,0 +1,142 @@
+# Author     : Jonathan Decker "Lokno"
+# Description: Joins an IRC channel and parses the chat messages for
+# percentages. It writes the average percentage from each unique
+# user to a file. Idle contributions are removed after a number of 
+# seconds, as defined by the variable lifeTime declared on line 29
+#
+# Reference: https://twitchtv.desk.com/customer/en/portal/articles/1302780-twitch-irc
+
+import socket
+import re,os,time
+import wave
+import configparser
+
+# Loading configuration file (ini format)
+config = configparser.ConfigParser(inline_comment_prefixes=';',)
+
+configFile = 'internalization.cfg'
+if not config.read(configFile):
+   print('ERROR: could not open file %s' % configFile)
+   sys.exit(-1)
+
+server   = config.get('MAIN','server')
+channel  = '#' + config.get('MAIN','channel')
+botnick  = config.get('MAIN','botnick')
+password = config.get('MAIN','password')
+usePygame = config.get('MAIN','usePygame', fallback=False)
+updateMapInterval = config.getint('TIMING','updateMapInterval')
+lifeTime          = config.getint('TIMING','lifeTime')
+cooldownSoundfile = config.getint('TIMING','cooldownSoundfile')
+percentOf = config.get('FILES','percentOf')
+filePath  = config.get('FILES','filePath')
+soundFilePath = config.get('FILES','goodPlayFilePath')
+
+percRE = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%")
+nameRE = re.compile("^:([^!]+)!")
+
+# Attempt to use pygame to load audio file
+if usePygame:
+   load_issue = False
+   try:
+      from pygame import mixer
+      from pygame import error as pyerr
+      mixer.init()
+      try:
+         sound = mixer.Sound(soundFilePath)
+      except pyerr:
+         print("ERROR: Could not load file %s. 32-bit WAVs are not supported." % soundFilePath)
+         load_issue = True
+   except ModuleNotFoundError:
+      print("ERROR: Could not load pygame.mixer. Did you install it? >> python -m pip install pygame")
+      load_issue = True
+
+   if load_issue:
+      print("Issue loading, defaulting to System.Media.SoundPlayer playback...")
+      usePygame = False
+
+def writefile(percent):
+   with open(filePath,'w') as f:
+      f.write("%s: %d%%" % (percentOf,percent))  
+
+def getAvg(sum,count):
+   avg = 0
+   if count > 0:
+      avg = sum/count
+   return avg
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("connecting to:" + server)
+irc.connect((server, 6667))
+print("connected!")
+print("joining channel '%s' as '%s'..." % (channel,botnick))
+
+irc.send(("PASS " + password + "\n").encode())
+irc.send(("NICK " + botnick  + "\n").encode())
+irc.send(("JOIN " + channel  + "\n").encode())
+
+initialVal = 0
+lastCheck  = time.time()
+voteMap    = {}
+vsum       = 0
+count      = 0
+changed    = True
+lastCooldownCheck = 0
+
+writefile(0)
+
+while 1:
+   text     = irc.recv(2040)
+   currTime = time.time()
+
+   percM = percRE.search(text.decode())
+   nameM = nameRE.search(text.decode())
+
+   if nameM and percM:
+      # nonsense numbers from chat are converted
+      # to integers in the range [0,100]
+      # what does 0.5% internalization even mean?
+      # also this can be -inf or inf, which is fun
+      val = float(percM.group(1))
+      val = int(max(0,min(val,100)))
+
+      currName = nameM.group(1)
+
+      # update sum, count and map
+      if currName in voteMap:
+         vsum -= voteMap[currName][0]
+      else:
+         count += 1
+      vsum += val
+      voteMap[currName] = [val,currTime]
+
+      percent = getAvg(vsum,count)
+      writefile(percent)
+
+      if soundFilePath != "":
+         # plays a sound file at 100% is it wasn't 100% before this vote and some
+         # period of time has past since the last time it played
+         if percent == 100 and changed and (currTime-lastCooldownCheck) > cooldownSoundfile:
+            if usePygame:
+               that_was_good.play()
+            else:
+               os.system('powershell -c (New-Object Media.SoundPlayer "%s").PlaySync();' % soundFilePath)   
+            lastCooldownCheck = currTime
+         elif percent != 100:
+            changed = True
+
+   # refreshes the map to remove votes from idle users
+   if (currTime-lastCheck) > updateMapInterval:
+      lastCheck = currTime
+      for k,v in list(voteMap.items()):
+         if (currTime-v[1]) >= lifeTime:
+            del voteMap[k]
+            vsum  -= v[0]
+            count -= 1
+
+      percent = getAvg(vsum,count)
+      writefile(percent)
+
+   # sends 'PONG' if 'PING' received to prevent pinging out
+   if text.find('PING'.encode()) != -1: 
+      irc.send(('PONG ' + text.split() [1] + '\r\n').encode())
---
+++ idle_two.py
@@ -0,0 +1,153 @@
+# Author     : Jonathan Decker "Lokno"
+# Description: Joins an IRC channel and parses the chat messages for
+# percentages. It writes the average percentage from each unique
+# user to a file. Idle contributions are removed after a number of 
+# seconds, as defined by the variable lifeTime declared on line 19
+# 
+# Two Player Variant - type dd%a for Player A and dd%b for Player B
+# dd% without a letter will default to Player A.
+#
+# Reference: https://twitchtv.desk.com/customer/en/portal/articles/1302780-twitch-irc
+ 
+import socket
+import string
+import re,os,time
+ 
+server   = "irc.chat.twitch.tv"
+channel  = "#CHANNEL"
+botnick  = "ACCOUNTNAME"
+password = "OAUTHTOKEN"
+letterA = 'a'
+letterB = 'b'
+filePathA = "internalization.txt"
+filePathB = "internalization_b.txt"
+
+updateMapInterval = 60
+lifeTime          = 60*3  # Time before a user's vote expires
+
+letterA = letterA.lower()
+letterB = letterB.lower()
+
+if letterA not in string.ascii_lowercase:
+   letterA = 'a'
+if letterB not in string.ascii_lowercase:
+   letterB = 'b'
+
+matchSetA = letterA + letterA.upper()
+matchSetB = letterB + letterB.upper()
+  
+percA_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([%s]?)(?![jJ])" % matchSetA)
+percB_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([%s])" % matchSetB)
+nameRE     = re.compile("^:([^!]+)!")
+
+def writefile(aN, aP, bN, bP):
+   with open(filePathA,'w') as f:
+      f.write("%s: %d%%" % (aN,aP))
+   with open(filePathB,'w') as f:
+      f.write("%s: %d%%" % (bN,bP)) 
+ 
+# Return of Ganon Font - https://zeldauniverse.net/media/fonts/
+def writeMeterFiles(aN, aP, bN, bP, max_hearts):
+   aFull = int(aP) / (100/max_hearts)
+   bFull = int(bP) / (100/max_hearts)
+   with open(filePathNick,'w') as f:
+      f.write(aN + ": " + "#" * aFull + "*" * (max_hearts-aFull))
+   with open(filePathJanel,'w') as f:
+      f.write(bN + ": " + "#" * bFull + "*" * (max_hearts-bFull))  
+ 
+def getAvg(sum,count):
+   avg = 0
+   if count > 0:
+      avg = sum/count
+   return avg
+ 
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+print("connecting to:" + server)
+irc.connect((server, 6667))
+print("connected!")
+print("joining channel %s as %s..." % (channel,botnick))
+ 
+irc.send(("PASS " + password + "\n").encode())
+irc.send(("NICK " + botnick  + "\n").encode())
+irc.send(("JOIN " + channel  + "\n").encode())
+ 
+lastCheck  = time.time()
+ 
+voteMapA = {}
+vsumA    = 0
+countA   = 0
+ 
+voteMapB = {}
+vsumB    = 0
+countB   = 0
+ 
+writefile(0,0)
+ 
+while 1:
+   text     = irc.recv(2040)
+   currTime = time.time()
+ 
+   percAM = percA_RE.search(text.decode())
+   percBM = percB_RE.search(text.decode())
+   nameM  = nameRE.search(text.decode())
+ 
+   if nameM and (percAM or percBM):
+      matches = []
+      if percAM:
+         matches.append((percAM.group(1),percAM.group(2)))
+      if percBM:
+         matches.append((percBM.group(1),percBM.group(2)))
+ 
+      for percM in matches:
+         # nonsense numbers from chat are converted
+         # to integers in the range [0,100]
+         # what does 0.5% internalization even mean?
+         # also this can be -inf or inf, which is fun
+         whoStr = letterA
+         if percM[1] != '':
+            whoStr = percM[1].lower()
+         val = float(percM[0])
+         val = int(max(0,min(val,100)))
+ 
+         currName = nameM.group(1)
+ 
+         # update sum, count and map
+         if( whoStr == letterA ):
+            if currName in voteMapA:
+               vsumA -= voteMapA[currName][0]
+            else:
+               countA += 1
+            vsumA += val
+            voteMapA[currName] = [val,currTime]
+         else:
+            if currName in voteMapB:
+               vsumB -= voteMapB[currName][0]
+            else:
+               countB += 1
+            vsumB += val
+            voteMapB[currName] = [val,currTime]
+ 
+      writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
+ 
+   # refreshes the map to remove votes from idle users
+   if (currTime-lastCheck) > updateMapInterval:
+      lastCheck = currTime
+ 
+      for k,v in list(voteMapA.items()):
+         if (currTime-v[1]) >= lifeTime:
+            del voteMapA[k]
+            vsumA  -= v[0]
+            countA -= 1
+ 
+      for k,v in list(voteMapB.items()):
+         if (currTime-v[1]) >= lifeTime:
+            del voteMapB[k]
+            vsumB  -= v[0]
+            countB -= 1
+ 
+      writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
+     
+   # sends 'PONG' if 'PING' received to prevent pinging out
+   if text.find('PING'.encode()) != -1: 
+      irc.send(('PONG ' + text.split() [1] + '\r\n').encode())
