7a3aa596e9e1c56624de9f9dec5490b0e0dde694
Lokno Decker <lokno1@gmail.com>
2020-01-12 21:26:06 -0500

    Added logging to both idle scripts; fixed bug with PONG

--- idle.py
+++ idle.py
@@ -7,52 +7,10 @@
 # Reference: https://twitchtv.desk.com/customer/en/portal/articles/1302780-twitch-irc
 
 import socket
-import re,os,time
+import re,os,time,sys
 import wave
 import configparser
-
-# Loading configuration file (ini format)
-config = configparser.ConfigParser(inline_comment_prefixes=';',)
-
-configFile = 'internalization.cfg'
-if not config.read(configFile):
-   print('ERROR: could not open file %s' % configFile)
-   sys.exit(-1)
-
-server   = config.get('MAIN','server')
-channel  = '#' + config.get('MAIN','channel')
-botnick  = config.get('MAIN','botnick')
-password = config.get('MAIN','password')
-usePygame = config.get('MAIN','usePygame', fallback=False)
-updateMapInterval = config.getint('TIMING','updateMapInterval')
-lifeTime          = config.getint('TIMING','lifeTime')
-cooldownSoundfile = config.getint('TIMING','cooldownSoundfile')
-percentOf = config.get('FILES','percentOf')
-filePath  = config.get('FILES','filePath')
-soundFilePath = config.get('FILES','goodPlayFilePath')
-
-percRE = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%")
-nameRE = re.compile("^:([^!]+)!")
-
-# Attempt to use pygame to load audio file
-if usePygame:
-   load_issue = False
-   try:
-      from pygame import mixer
-      from pygame import error as pyerr
-      mixer.init()
-      try:
-         sound = mixer.Sound(soundFilePath)
-      except pyerr:
-         print("ERROR: Could not load file %s. 32-bit WAVs are not supported." % soundFilePath)
-         load_issue = True
-   except ModuleNotFoundError:
-      print("ERROR: Could not load pygame.mixer. Did you install it? >> python -m pip install pygame")
-      load_issue = True
-
-   if load_issue:
-      print("Issue loading, defaulting to System.Media.SoundPlayer playback...")
-      usePygame = False
+import traceback
 
 def writefile(percent):
    with open(filePath,'w') as f:
@@ -64,79 +22,128 @@ def getAvg(sum,count):
       avg = sum/count
    return avg
 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+   # Loading configuration file (ini format)
+   config = configparser.ConfigParser(inline_comment_prefixes=';',)
 
-print("connecting to:" + server)
-irc.connect((server, 6667))
-print("connected!")
-print("joining channel '%s' as '%s'..." % (channel,botnick))
+   configFile = 'internalization.cfg'
+   if not config.read(configFile):
+      print('ERROR: could not open file %s' % configFile)
+      sys.exit(-1)
 
-irc.send(("PASS " + password + "\n").encode())
-irc.send(("NICK " + botnick  + "\n").encode())
-irc.send(("JOIN " + channel  + "\n").encode())
+   server   = config.get('MAIN','server')
+   channel  = '#' + config.get('MAIN','channel')
+   botnick  = config.get('MAIN','botnick')
+   password = config.get('MAIN','password')
+   usePygame = config.get('MAIN','usePygame', fallback=False)
+   updateMapInterval = config.getint('TIMING','updateMapInterval')
+   lifeTime          = config.getint('TIMING','lifeTime')
+   cooldownSoundfile = config.getint('TIMING','cooldownSoundfile')
+   percentOf = config.get('FILES','percentOf')
+   filePath  = config.get('FILES','filePath')
+   soundFilePath = config.get('FILES','goodPlayFilePath')
 
-initialVal = 0
-lastCheck  = time.time()
-voteMap    = {}
-vsum       = 0
-count      = 0
-changed    = True
-lastCooldownCheck = 0
+   percRE = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%")
+   nameRE = re.compile("^:([^!]+)!")
 
-writefile(0)
+   # Attempt to use pygame to load audio file
+   if usePygame:
+      load_issue = False
+      try:
+         from pygame import mixer
+         from pygame import error as pyerr
+         mixer.init()
+         try:
+            sound = mixer.Sound(soundFilePath)
+         except pyerr:
+            print("ERROR: Could not load file %s. 32-bit WAVs are not supported." % soundFilePath)
+            load_issue = True
+      except ModuleNotFoundError:
+         print("ERROR: Could not load pygame.mixer. Did you install it? >> python -m pip install pygame")
+         load_issue = True
 
-while 1:
-   text     = irc.recv(2040)
-   currTime = time.time()
+      if load_issue:
+         print("Issue loading, defaulting to System.Media.SoundPlayer playback...")
+         usePygame = False
 
-   percM = percRE.search(text.decode())
-   nameM = nameRE.search(text.decode())
+   irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-   if nameM and percM:
-      # nonsense numbers from chat are converted
-      # to integers in the range [0,100]
-      # what does 0.5% internalization even mean?
-      # also this can be -inf or inf, which is fun
-      val = float(percM.group(1))
-      val = int(max(0,min(val,100)))
+   print("connecting to:" + server)
+   irc.connect((server, 6667))
+   print("connected!")
+   print("joining channel '%s' as '%s'..." % (channel,botnick))
 
-      currName = nameM.group(1)
+   irc.send(("PASS " + password + "\n").encode())
+   irc.send(("NICK " + botnick  + "\n").encode())
+   irc.send(("JOIN " + channel  + "\n").encode())
 
-      # update sum, count and map
-      if currName in voteMap:
-         vsum -= voteMap[currName][0]
-      else:
-         count += 1
-      vsum += val
-      voteMap[currName] = [val,currTime]
+   initialVal = 0
+   lastCheck  = time.time()
+   voteMap    = {}
+   vsum       = 0
+   count      = 0
+   changed    = True
+   lastCooldownCheck = 0
 
-      percent = getAvg(vsum,count)
-      writefile(percent)
+   writefile(0)
 
-      if soundFilePath != "":
-         # plays a sound file at 100% is it wasn't 100% before this vote and some
-         # period of time has past since the last time it played
-         if percent == 100 and changed and (currTime-lastCooldownCheck) > cooldownSoundfile:
-            if usePygame:
-               that_was_good.play()
-            else:
-               os.system('powershell -c (New-Object Media.SoundPlayer "%s").PlaySync();' % soundFilePath)   
-            lastCooldownCheck = currTime
-         elif percent != 100:
-            changed = True
+   while 1:
+      text     = irc.recv(2040)
+      currTime = time.time()
 
-   # refreshes the map to remove votes from idle users
-   if (currTime-lastCheck) > updateMapInterval:
-      lastCheck = currTime
-      for k,v in list(voteMap.items()):
-         if (currTime-v[1]) >= lifeTime:
-            del voteMap[k]
-            vsum  -= v[0]
-            count -= 1
+      percM = percRE.search(text.decode())
+      nameM = nameRE.search(text.decode())
 
-      percent = getAvg(vsum,count)
-      writefile(percent)
+      if nameM and percM:
+         # nonsense numbers from chat are converted
+         # to integers in the range [0,100]
+         # what does 0.5% internalization even mean?
+         # also this can be -inf or inf, which is fun
+         val = float(percM.group(1))
+         val = int(max(0,min(val,100)))
 
-   # sends 'PONG' if 'PING' received to prevent pinging out
-   if text.find('PING'.encode()) != -1: 
-      irc.send(('PONG ' + text.split() [1] + '\r\n').encode())
+         currName = nameM.group(1)
+
+         # update sum, count and map
+         if currName in voteMap:
+            vsum -= voteMap[currName][0]
+         else:
+            count += 1
+         vsum += val
+         voteMap[currName] = [val,currTime]
+
+         percent = getAvg(vsum,count)
+         writefile(percent)
+
+         if soundFilePath != "":
+            # plays a sound file at 100% is it wasn't 100% before this vote and some
+            # period of time has past since the last time it played
+            if percent == 100 and changed and (currTime-lastCooldownCheck) > cooldownSoundfile:
+               if usePygame:
+                  that_was_good.play()
+               else:
+                  os.system('powershell -c (New-Object Media.SoundPlayer "%s").PlaySync();' % soundFilePath)   
+               lastCooldownCheck = currTime
+            elif percent != 100:
+               changed = True
+
+      # refreshes the map to remove votes from idle users
+      if (currTime-lastCheck) > updateMapInterval:
+         lastCheck = currTime
+         for k,v in list(voteMap.items()):
+            if (currTime-v[1]) >= lifeTime:
+               del voteMap[k]
+               vsum  -= v[0]
+               count -= 1
+
+         percent = getAvg(vsum,count)
+         writefile(percent)
+
+      # sends 'PONG' if 'PING' received to prevent pinging out
+      if text.find('PING'.encode()) != -1: 
+         irc.send(('PONG ' + text.decode().split() [1] + '\r\n').encode())
+         
+except:
+   with open('idle_log.txt','w') as f:
+      print("EXCEPTION OCCURRED: Aborting %s. See idle_log.txt" % sys.argv[0])
+      f.write(traceback.print_exc(file=f))
--- idle_two.py
+++ idle_two.py
@@ -11,34 +11,8 @@
  
 import socket
 import string
-import re,os,time
- 
-server   = "irc.chat.twitch.tv"
-channel  = "#CHANNEL"
-botnick  = "ACCOUNTNAME"
-password = "OAUTHTOKEN"
-letterA = 'a'
-letterB = 'b'
-filePathA = "internalization.txt"
-filePathB = "internalization_b.txt"
-
-updateMapInterval = 60
-lifeTime          = 60*3  # Time before a user's vote expires
-
-letterA = letterA.lower()
-letterB = letterB.lower()
-
-if letterA not in string.ascii_lowercase:
-   letterA = 'a'
-if letterB not in string.ascii_lowercase:
-   letterB = 'b'
-
-matchSetA = letterA + letterA.upper()
-matchSetB = letterB + letterB.upper()
-  
-percA_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([%s]?)(?![jJ])" % matchSetA)
-percB_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([%s])" % matchSetB)
-nameRE     = re.compile("^:([^!]+)!")
+import re,os,time,sys
+import traceback
 
 def writefile(aN, aP, bN, bP):
    with open(filePathA,'w') as f:
@@ -60,94 +34,127 @@ def getAvg(sum,count):
    if count > 0:
       avg = sum/count
    return avg
- 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-print("connecting to:" + server)
-irc.connect((server, 6667))
-print("connected!")
-print("joining channel %s as %s..." % (channel,botnick))
- 
-irc.send(("PASS " + password + "\n").encode())
-irc.send(("NICK " + botnick  + "\n").encode())
-irc.send(("JOIN " + channel  + "\n").encode())
- 
-lastCheck  = time.time()
- 
-voteMapA = {}
-vsumA    = 0
-countA   = 0
- 
-voteMapB = {}
-vsumB    = 0
-countB   = 0
- 
-writefile(0,0)
- 
-while 1:
-   text     = irc.recv(2040)
-   currTime = time.time()
- 
-   percAM = percA_RE.search(text.decode())
-   percBM = percB_RE.search(text.decode())
-   nameM  = nameRE.search(text.decode())
- 
-   if nameM and (percAM or percBM):
-      matches = []
-      if percAM:
-         matches.append((percAM.group(1),percAM.group(2)))
-      if percBM:
-         matches.append((percBM.group(1),percBM.group(2)))
- 
-      for percM in matches:
-         # nonsense numbers from chat are converted
-         # to integers in the range [0,100]
-         # what does 0.5% internalization even mean?
-         # also this can be -inf or inf, which is fun
-         whoStr = letterA
-         if percM[1] != '':
-            whoStr = percM[1].lower()
-         val = float(percM[0])
-         val = int(max(0,min(val,100)))
- 
-         currName = nameM.group(1)
- 
-         # update sum, count and map
-         if( whoStr == letterA ):
-            if currName in voteMapA:
-               vsumA -= voteMapA[currName][0]
-            else:
-               countA += 1
-            vsumA += val
-            voteMapA[currName] = [val,currTime]
-         else:
-            if currName in voteMapB:
-               vsumB -= voteMapB[currName][0]
-            else:
-               countB += 1
-            vsumB += val
-            voteMapB[currName] = [val,currTime]
- 
-      writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
- 
-   # refreshes the map to remove votes from idle users
-   if (currTime-lastCheck) > updateMapInterval:
-      lastCheck = currTime
- 
-      for k,v in list(voteMapA.items()):
-         if (currTime-v[1]) >= lifeTime:
-            del voteMapA[k]
-            vsumA  -= v[0]
-            countA -= 1
- 
-      for k,v in list(voteMapB.items()):
-         if (currTime-v[1]) >= lifeTime:
-            del voteMapB[k]
-            vsumB  -= v[0]
-            countB -= 1
- 
-      writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
+
+try:
+   server   = "irc.chat.twitch.tv"
+   channel  = "#CHANNEL"
+   botnick  = "ACCOUNTNAME"
+   password = "OAUTHTOKEN"
+   letterA = 'a'
+   letterB = 'b'
+   filePathA = "internalization.txt"
+   filePathB = "internalization_b.txt"
+
+   updateMapInterval = 60
+   lifeTime          = 60*3  # Time before a user's vote expires
+
+   letterA = letterA.lower()
+   letterB = letterB.lower()
+
+   if letterA not in string.ascii_lowercase:
+      letterA = 'a'
+   if letterB not in string.ascii_lowercase:
+      letterB = 'b'
+
+   matchSetA = letterA + letterA.upper()
+   matchSetB = letterB + letterB.upper()
      
-   # sends 'PONG' if 'PING' received to prevent pinging out
-   if text.find('PING'.encode()) != -1: 
-      irc.send(('PONG ' + text.split() [1] + '\r\n').encode())
+   percA_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([" + matchSetA + "]?)(?![jJ])")
+   percB_RE   = re.compile("(?<![\d\.])(-?\d+\.?\d*)\%([" + matchSetB + "])")
+   nameRE     = re.compile("^:([^!]+)!")
+    
+   irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+   print("connecting to:" + server)
+   irc.connect((server, 6667))
+   print("connected!")
+   print("joining channel %s as %s..." % (channel,botnick))
+    
+   irc.send(("PASS " + password + "\n").encode())
+   irc.send(("NICK " + botnick  + "\n").encode())
+   irc.send(("JOIN " + channel  + "\n").encode())
+    
+   lastCheck  = time.time()
+    
+   voteMapA = {}
+   vsumA    = 0
+   countA   = 0
+    
+   voteMapB = {}
+   vsumB    = 0
+   countB   = 0
+    
+   writefile('A',0,'B',0)
+
+   while 1:
+      text     = irc.recv(2040)
+      currTime = time.time()
+    
+      percAM = percA_RE.search(text.decode())
+      percBM = percB_RE.search(text.decode())
+      nameM  = nameRE.search(text.decode())
+    
+      if nameM and (percAM or percBM):
+         matches = []
+         if percAM:
+            matches.append((percAM.group(1),percAM.group(2)))
+         if percBM:
+            matches.append((percBM.group(1),percBM.group(2)))
+    
+         for percM in matches:
+            # nonsense numbers from chat are converted
+            # to integers in the range [0,100]
+            # what does 0.5% internalization even mean?
+            # also this can be -inf or inf, which is fun
+            whoStr = letterA
+            if percM[1] != '':
+               whoStr = percM[1].lower()
+            val = float(percM[0])
+            val = int(max(0,min(val,100)))
+    
+            currName = nameM.group(1)
+    
+            # update sum, count and map
+            if( whoStr == letterA ):
+               if currName in voteMapA:
+                  vsumA -= voteMapA[currName][0]
+               else:
+                  countA += 1
+               vsumA += val
+               voteMapA[currName] = [val,currTime]
+            else:
+               if currName in voteMapB:
+                  vsumB -= voteMapB[currName][0]
+               else:
+                  countB += 1
+               vsumB += val
+               voteMapB[currName] = [val,currTime]
+    
+         writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
+    
+      # refreshes the map to remove votes from idle users
+      if (currTime-lastCheck) > updateMapInterval:
+         lastCheck = currTime
+    
+         for k,v in list(voteMapA.items()):
+            if (currTime-v[1]) >= lifeTime:
+               del voteMapA[k]
+               vsumA  -= v[0]
+               countA -= 1
+    
+         for k,v in list(voteMapB.items()):
+            if (currTime-v[1]) >= lifeTime:
+               del voteMapB[k]
+               vsumB  -= v[0]
+               countB -= 1
+    
+         writefile(getAvg(vsumA,countA),getAvg(vsumB,countB))
+        
+      # sends 'PONG' if 'PING' received to prevent pinging out
+      if text.find('PING'.encode()) != -1: 
+         irc.send(('PONG ' + text.decode().split() [1] + '\r\n').encode())
+         
+except:
+   with open('idle_log.txt','w') as f:
+      print("EXCEPTION OCCURRED: Aborting %s. See idle_log.txt" % sys.argv[0])
+      traceback.print_exc(file=f)
